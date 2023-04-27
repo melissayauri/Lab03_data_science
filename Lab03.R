1cf9d3d3490fdd7bba383ae99aab3fc670ee2ebd
melissayauri <yaurimelissa@gmail.ccom>
2023-04-27 17:07:58 -0500

    Conversión de datos de las columnas Method, Protocol, Response_code y Bytes

--- Lab03.R
+++ Lab03.R
@@ -11,3 +11,27 @@ library(stringr)
 server_data <- read_table("D:\\MAESTRIA-CIBERSEGURIDAD\\SEMINARIO_DATA_SCIENCE\\LABS\\Lab03_lengR\\epa-http.csv", col_names = FALSE)
 # Renonbrando los encabezados de las columnas
 colnames(server_data) <- c("Ip_address", "Date", "Method", "Resource", "Protocol", "Response_code", "Bytes")
+
+# PREGUNTA 1
+# Conversión de datos
+
+# Formateando la columna Method de "\"GET" a "GET"
+server_data$Method <- str_sub(server_data$Method, 2)
+# Convirtiendo a datos categóricos de la columna Method
+server_data$Method <- as.factor(server_data$Method)
+
+# Formateando la columna Protocol de "HTTP/1.0\"" a "HTTP/1.0"
+server_data$Protocol <- str_sub(server_data$Protocol, end = -2)
+# Convirtiendo a datos categóricos
+server_data$Protocol <- as.factor(server_data$Protocol)
+
+# Convirtiendo a datos catégoricos la columna Response_code
+server_data$Response_code <- as.factor(server_data$Response_code)
+
+# Formateando la columna Bytes,reemplazando "-" por 0
+server_data$Bytes <- str_replace(server_data$Bytes, "-", "0")
+# Convirtiendo a datos númericos
+server_data$Bytes <- as.numeric(server_data$Bytes)
+
+
+View(server_data)
