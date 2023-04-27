e217c3c1e270ba683eecba4d08e7d9b8ae643154
melissayauri <yaurimelissa@gmail.ccom>
2023-04-27 16:50:56 -0500

    Importando archivo y renombrando columnas

--- Lab03.R
+++ Lab03.R
@@ -6,3 +6,8 @@ library(ggplot2)
 library(data.table)
 library(mltools)
 library(stringr)
+
+# Importando el archivo
+server_data <- read_table("D:\\MAESTRIA-CIBERSEGURIDAD\\SEMINARIO_DATA_SCIENCE\\LABS\\Lab03_lengR\\epa-http.csv", col_names = FALSE)
+# Renonbrando los encabezados de las columnas
+colnames(server_data) <- c("Ip_address", "Date", "Method", "Resource", "Protocol", "Response_code", "Bytes")
