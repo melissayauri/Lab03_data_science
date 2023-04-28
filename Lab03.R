c685e3625de8c6dd6d26f787547f1593eeb347cf
melissayauri <yaurimelissa@gmail.ccom>
2023-04-27 22:21:15 -0500

    Añadiendo gráficas

--- Lab03.R
+++ Lab03.R
@@ -101,4 +101,54 @@ print(method2_data)
 # HEAD       106
 # POST      1622
 
+# Pregunta 4
+# Data
+data_ta <- data.frame(Method = server_data$Method, Response_code = server_data$Response_code)
+# Frequencia según la columna Method y Response_code
+requests_freq <- table(server_data$Method, server_data$Response_code)
+# Conversión a data.frame
+requests_data <- as.data.frame(requests_freq)
+# Añadiendo nombres a la tabla
+colnames(requests_data) <- c("Method", "Response_code", "Frequency")
+# Nueva data para validarlo con la gráfica
+new_data <- spread(requests_data, Response_code, Frequency)
+#requests_data$Frequency <- as.numeric(requests_data$Frequency)
+
+# Creando los gráficos
+
+# Gráfica 1
+ggplot(requests_data, aes(x = Method, y = Frequency, fill = Method)) + 
+  geom_bar(stat="identity", position="dodge") +
+  scale_fill_manual(values=c("#a40b54", "#11766d", "#027fe9"), 
+                    labels=c("GET", "HEAD", "POST")) +
+  labs(title = "Código de respuesta con el tipo de método", 
+       x = "Método de solicitud", y = "Frecuencia", 
+       fill = "Método") +
+  facet_wrap(~Response_code, ncol = 3, scales = "free_y") +
+   geom_text(aes(label = ..y..), stat="identity", position=position_dodge(width=1), vjust=-0.5, size=3) +
+  scale_y_continuous(expand = expansion(mult = c(0.05, 0.1), add = c(0, 10)))
+ 
+# Gráfica 2
+ggplot(requests_data, aes(x = Response_code, y = Frequency, fill = Method)) + 
+  geom_bar(stat="identity") +
+  scale_fill_manual(values=c("#a40b54", "#11766d", "#027fe9"), 
+                    labels=c("GET", "HEAD", "POST")) +
+  labs(title = "Método según el código de respuesta", 
+       x = "Código de respuesta", y = "Frecuencia", 
+       fill = "Método") +
+  facet_wrap(~Method, ncol = 1, scales = "free_y") +
+  geom_text(aes(label = ..y..), stat="identity", position=position_stack(vjust=0.5), size=3)
+
+# Gráfica 3
+ggplot(requests_data, aes(x = Method, y = Frequency, color = Response_code)) +
+  geom_point() +
+  geom_text(aes(label = Frequency), vjust = -1) +
+  labs(title = "Método de solicitud según código de respuesta", x = "Método de solicitud", y = "Frecuencia", color = "Código de respuesta") +
+  facet_grid(Response_code ~ .) +
+  ylim(0, 60000) +
+  scale_color_manual(values=c("#f70a71", "#c90a02", "#ffb145", "#74ab90", "#3a89c9","#1b325f","#f26c4f","#63203d"), 
+                     labels=c("200", "302","304","400","403","404", "500", "501"))
+
+
+
 View(server_data)
